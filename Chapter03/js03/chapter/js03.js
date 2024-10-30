43ea5605f114f204623b04aea8e48ae554d1eeb1
CyrusWebSolutions <cyruswebsolutions@gmail.com>
2024-10-30 02:54:07 -0400

    Finished Ch.3 Reading and Questions

--- Chapter03/js03/chapter/js03.js
+++ Chapter03/js03/chapter/js03.js
@@ -22,3 +22,53 @@ function addWeekdays() {
         i++;
     }
 }
+
+window.addEventListener("load", showGames);
+
+function showGames() {
+    for (let i = 0; i < gameDates.length; i++) {
+        let gameInfo = "";
+
+        switch (gameResults[i]) {
+            case "W":
+                gameInfo += "<p class='win'>";
+                break;
+            case "L":
+                gameInfo += "<p class='lose'>";
+                break;     
+            case "S":
+                gameInfo += "<p class='suspended'>";
+                break;
+            case "P":
+                gameInfo += "<p class='postponed'>";
+                break;               
+        }
+
+        if (gameLocations[i] === "h") {
+            gameInfo += "vs. ";
+
+        } else if (gameLocations[i] === "a") {
+            gameInfo += "@ ";
+        }
+
+        gameInfo += gameOpponents[i] + "<br>";
+
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + " - " + runsAllowed[i] + ")";
+
+        if (gameInnings[i] < 5) {
+            gameInfo += " [" + gameInnings[i]+"]***";
+        } else if (gameInnings[i] < 9) {
+            gameInfo += " [" + gameInnings[i]+"]*";
+        } else if (gameInnings[i] > 9) {
+            gameInfo += " [" + gameInnings[i]+ "]";
+        }
+
+        gameInfo += "</p>";
+
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo)
+
+    }
+
+
+}
